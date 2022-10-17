372c51513f23b016aa3e4b523c7b0d8231fe9aa6
christianSr93 <christian.sanchez.93@gmail.com>
2022-10-17 11:20:52 -0500

    refactor: move useEffects to custom hook

--- src/Components/ListCards/listCards.jsx
+++ src/Components/ListCards/listCards.jsx
@@ -1,43 +1,18 @@
-import React, {useEffect, useState} from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 import './listCards.scss';
 import Cards from '../Cards/cards'
 import Grid from '@mui/material/Grid';
-import {getCharacters} from '../../API/getCharacters';
-
-
-
+import useListCards from './hooks/useListCards';
 const ListCards = props => {
-
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    let mounted = true;
-
-    if(mounted){
-      getCharacters()
-      .then(data => {
-        setCharacters(data)
-      })
-    }
-    return () => mounted = false;
-  }, []);
-
-  useEffect(()=>{
-    console.log("🚀 ~ file: listCards.jsx ~ line 29 ~ ListCards ~ characters", characters)
-  },[characters])
+ const {filterCharacters} = useListCards();
 
   return (
     <Grid container spacing={2}>
-      {characters.map(e=>(
+      {filterCharacters.map(e=>(
         <Cards data={e}></Cards>
       ))}
     </Grid>
   )
-}
-
-ListCards.propTypes = {
-
 }
 
 export default ListCards
