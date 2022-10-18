bc44364e01e3a25d156c32a953399541f03ce654
christianSr93 <christian.sanchez.93@gmail.com>
2022-10-18 18:51:07 -0500

    refactor: change return in fetch

--- src/API/saveFile.js
+++ src/API/saveFile.js
@@ -2,7 +2,7 @@ export async function saveFile(selectedFile) {
     const formData = new FormData();
     formData.append('image', selectedFile);
     const url = `https://api.imgbb.com/1/upload?key=64b77e3d2a70760a2d759c376a806ecc`;
-    return fetch(
+    return await fetch(
 			url,
 			{
 				method: 'POST',
@@ -10,10 +10,5 @@ export async function saveFile(selectedFile) {
 			}
 		)
 			.then((response) => response.json())
-			.then((result) => {
-				console.log('Success:', result);
-			})
-			.catch((error) => {
-				console.error('Error:', error);
-			});
+			
   }
