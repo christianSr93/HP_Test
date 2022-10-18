dfd4eff85f8c2718902fd141e93795a9a7265aa6
christianSr93 <christian.sanchez.93@gmail.com>
2022-10-18 18:33:37 -0500

    fix: change key to the url

--- src/API/saveFile.js
+++ src/API/saveFile.js
@@ -1,11 +1,10 @@
 export async function saveFile(selectedFile) {
     const formData = new FormData();
     formData.append('File', selectedFile);
-    const url = `https://api.imgbb.com/1/upload`;
+    const url = `https://api.imgbb.com/1/upload?key=64b77e3d2a70760a2d759c376a806ecc`;
     return fetch(
 			url,
 			{
-				key: '64b77e3d2a70760a2d759c376a806ecc',
 				method: 'POST',
 				body: formData,
 			}
