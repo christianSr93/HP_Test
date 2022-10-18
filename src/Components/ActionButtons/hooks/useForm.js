414afcd46ab38bd5f3771ba860ce1c131688d01b
christianSr93 <christian.sanchez.93@gmail.com>
2022-10-18 18:57:47 -0500

    refactor: remove console log

--- src/Components/ActionButtons/hooks/useForm.js
+++ src/Components/ActionButtons/hooks/useForm.js
@@ -47,7 +47,6 @@ const useForm = (props) => {
     const handleSubmit = async(e) =>{
         e.preventDefault();
         const save = await saveFile(selectedFile);
-        console.log("🚀 ~ file: useForm.js ~ line 50 ~ handleSubmit ~ save", save)
         const data = {name, dateOfBirth: birthday, eyeColour:eyeColor, hairColour: hairColor, gender, hogwartsStudent: position === 'estudiante' ? true:false, hogwartsStaff:position === 'staff' ? true:false , image: save.data.url}
         const res = await addCharacter(data);
         res.status === 200 || res.status === 201 ? alert('Se agrego correctamente.'):alert('Oops, Algo salio mal.')    
