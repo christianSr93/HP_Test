488da31a1281f55c064605af66385dc83f59fbf7
christianSr93 <christian.sanchez.93@gmail.com>
2022-10-18 11:45:29 -0500

    feat: add ActionButtons component in the main render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import ListCards from './Components/ListCards/listCards';
 import Filter from './Components/Filters/filter'
+import ActionButtons from './Components/ActionButtons/actionButtons';
 
 function App() {
   return (
     <div className="App">
+      <ActionButtons></ActionButtons>
       <Filter></Filter>
       <ListCards/>
     </div>
