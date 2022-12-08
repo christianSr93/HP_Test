499aa21c8327b00125188547adb0d2606872d5c1
christianSr93 <christian.sanchez.93@gmail.com>
2022-12-08 14:12:37 -0600

    refactor: change functional component to class component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.scss';
 import ListCards from './Components/ListCards/listCards';
 import Filter from './Components/Filters/filter'
 import ActionButtons from './Components/ActionButtons/actionButtons';
+import ActionButtonClass from './Components/ActionButtons/actionButtonClass';
 
 function App() {
   return (
     <div className="App">
-      <ActionButtons></ActionButtons>
+      {/* <ActionButtons></ActionButtons> */}
+      <ActionButtonClass></ActionButtonClass>
       <Filter></Filter>
       <ListCards/>
     </div>
